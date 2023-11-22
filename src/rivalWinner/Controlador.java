dd64fc83c456dbf1d25c6bdb6edea7d178a7e075
Administrador <Administrador@2DAM01>
2023-11-22 18:39:37 +0100

    Creacion de ficheros

---
+++ src/rivalWinner/Controlador.java
@@ -0,0 +1,5 @@
+package rivalWinner;
+
+public class Controlador {
+
+}
---
+++ src/rivalWinner/RivalWinner.java
@@ -0,0 +1,10 @@
+package rivalWinner;
+
+public class RivalWinner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
